7eddde8e1050ae24e784f676a8522af62aeb9109
Mehmet <mhmtsytrk@gmail.com>
2020-10-15 03:26:28 +0300

    After Streams

---
+++ src/main/java/cybertek/streams/CreatingStream.java
@@ -0,0 +1,26 @@
+package cybertek.streams;
+
+import cybertek.collections.ArrayLists;
+import cybertek.oop.encapsulation.Role;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class CreatingStream {
+
+    public static void main(String[] args) {
+
+
+        //Creating stream from an Array
+        String[] courses = {"Java","JS","TS","API","SELENIUM"};
+        Stream<String> courseStream = Arrays.stream(courses);
+
+        //Creating a Stream from a Collection
+       //  Stream<Role> roleStream = ArrayLists.createRoleList().stream();
+
+        //Creating a Stream from Specified Values
+        Stream<Integer> stream = Stream.of(1,2,3,4,5,6);
+
+
+    }
+}
